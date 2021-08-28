b7049e51a7a0890d5e96a6f772adc2cd4040e5c5
Darcy LF <ehnevault@gmail.com>
2021-08-28 13:36:19 +1000

    :recycle: fix up travel calculator to work for routes

--- nonroutes.py
+++ nonroutes.py
@@ -11,30 +11,36 @@ def gen_hash(pass1, salt):
     m.update(salt.encode())
     return m.hexdigest()
 
-def calculator:
+def travel_calculator(start_location, end_location, co2_per_km):
     access_token = "sk.eyJ1IjoiYm9iMTIzMSIsImEiOiJja3N2M3BndjcxNTgwMm9xenFiNzZvdWxoIn0.mTgzAAZ_sb_oz60TSocXzg"
     perm_geocoder = Geocoder(name='mapbox.places-permanent')
     geocoder = Geocoder(access_token=access_token)
+    out = {}
 
-    start = input(str("Enter in Starting Address: "))
-    response1 = geocoder.forward(start)
+    response1 = geocoder.forward(start_location)
     first = response1.geojson()['features'][0]
-    print(f"{first['place_name']} {first['geometry']}")
+    out["firstPlace"] = first['place_name']
+    #print(f"{first['place_name']} {first['geometry']}")
 
-    destination = input(str("Enter in Destination: "))
-    response2 = geocoder.forward(destination)
+    
+    response2 = geocoder.forward(end_location)
     second = response2.geojson()['features'][0]
-    print(f"{second['place_name']} {second['geometry']}")
+    out["second_place"] = second['place_name']
+    #print(f"{second['place_name']} {second['geometry']}")
 
     service = Directions(access_token=access_token)
 
     response = service.directions([first, second],'mapbox/driving')
     global driving_routes
     driving_routes = response.geojson()
-    print(driving_routes)
+    #print(driving_routes)
 
-    co2_g_per_km = input(float("Enter in how much CO2 is given per km in grams: "))
+    #co2_g_per_km = input(float("Enter in how much CO2 is given per km in grams: "))
     distance_of_trip = driving_routes['distance']
-    total = distance_of_trip * co2_g_per_km
-    print(f"You will be usng {total} grams of CO2")
+    total = distance_of_trip * co2_per_km
+    #print(f"You will be usng {total} grams of CO2")
     # add something about those carbon tokens
+    out["total_co2"] = total
+    return out
+
+
