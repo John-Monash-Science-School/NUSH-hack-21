bcb55a059bc142e0eeb1af938c52551b51574735
Darcy LF <ehnevault@gmail.com>
2021-08-28 14:42:33 +1000

    :sparkles: make calculator work

--- nonroutes.py
+++ nonroutes.py
@@ -36,11 +36,13 @@ def travel_calculator(start_location, end_location, co2_per_km):
     #print(driving_routes)
 
     #co2_g_per_km = input(float("Enter in how much CO2 is given per km in grams: "))
-    distance_of_trip = driving_routes['distance']
+    #print(driving_routes)
+    distance_of_trip = driving_routes['features'][0]['properties']['distance']
     total = distance_of_trip * co2_per_km
     #print(f"You will be usng {total} grams of CO2")
     # add something about those carbon tokens
-    out["total_co2"] = total
+    # multiply to get in tonnes
+    out["total_co2"] = total * 0.000001
     return out
 
 
