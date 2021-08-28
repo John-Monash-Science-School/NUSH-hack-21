9852111de4c14784aaad249aa9e984a55eebc8e0
Darcy LF <ehnevault@gmail.com>
2021-08-28 12:07:15 +1000

    :sparkles: link to search in nav

--- components/Header.js
+++ components/Header.js
@@ -4,17 +4,17 @@ function Header () {
             <h1 class="text-xl"><a href="/" class="hover:opacity-80">Carbon Coins</a></h1>
             <div class="flex-grow"></div>
             <nav class="text-base">
-                <a href="/login" class="px-2 py-1 bg-b_med ml-2 hover:bg-b_high inline-flex items-center">
+                <a href="/login" class="px-2 py-1 bg-b_med ml-2 hover:bg-b_high inline-flex items-center content-center leading-none">
                     <span class="iconify mr-1 text-xl" data-icon="carbon:user-avatar"></span>
-                    login
+                    <span class="block">Login</span>
                 </a>
-                <a href="#" class="px-2 py-1 bg-b_med ml-2 hover:bg-b_high inline-flex items-center">
-                    <span class="iconify mr-1 text-xl" data-icon="carbon:share-knowledge"></span>
-                    trade
+                <a href="/search" class="px-2 py-1 bg-b_med ml-2 hover:bg-b_high inline-flex items-center content-center leading-none">
+                    <span class="iconify mr-1 text-xl" data-icon="carbon:search-locate"></span>
+                    <span class="block">Search</span>  
                 </a>
-                <a href="/calculator" class="px-2 py-1 bg-b_med ml-2 hover:bg-b_high inline-flex items-center">
+                <a href="/calculator" class="px-2 py-1 bg-b_med ml-2 hover:bg-b_high inline-flex items-center content-center leading-none">
                     <span class="iconify mr-1 text-xl" data-icon="carbon:calculator"></span>
-                    calculate
+                    <span class="block">Calculate</span>
                 </a>
             </nav>
         </header>
